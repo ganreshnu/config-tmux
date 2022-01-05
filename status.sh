85744ac130018e6c918723760624e86fafe8695f
Ganreshnu <ganreshnu@gmail.com>
2022-01-05 16:55:44 -0600

    Changed the status colors a bit.

--- status.sh
+++ status.sh
@@ -4,23 +4,23 @@ function main() {
 
 	pushd $HOME
 	if [[ -n "$(git -C .gnupg status --porcelain)" ]]; then
-		printf "#[fg=red]%s#[default] " "gnupg"
+		printf "#[fg=brightred]%s#[default] " "gnupg"
 	fi
 	if [[ -n "$(git -C .ssh status --porcelain)" ]]; then
-		printf "#[fg=red]%s#[default] " "openssh"
+		printf "#[fg=brightred]%s#[default] " "openssh"
 	fi
 
 	pushd .config
 	for i in $(ls); do
 		if [[ -n "$(git -C $i status --porcelain)" ]]; then
-			printf "#[fg=red]%s#[default] " $i
+			printf "#[fg=brightred]%s#[default] " $i
 		fi
 	done
 	popd # .config
 	popd # $HOME
 
-	printf "#[fg=green]C:%s#[default] " "$(docker ps --quiet | wc -l)"
-	printf "#[fg=yellow]%s#[default]%s" "$(hostname)" "$(date +'%l:%M%P')"
+	printf "#[fg=brightcyan]C:%s#[default] " "$(docker ps --quiet | wc -l)"
+	printf "#[fg=brightblack]%s %s#[default]" "$(hostname)" "$(date +'%l:%M%P')"
 
 }
 
