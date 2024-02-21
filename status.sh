dc4ca2e9f02541cf9559be0432d421739b0a191b
Ganreshnu <ganreshnu@gmail.com>
2024-02-21 13:47:52 -0600

    updated the status bar script

--- status.sh
+++ status.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 function check-git-status() {
-	local branch="$(git -C $1 branch --show-current)"
+	local branch="$(git -C "$1" branch --show-current)"
 	local style=""
 
-	if ! git -C $1 diff-files --quiet --ignore-submodules; then
+	if ! git -C "$1" diff-files --quiet --ignore-submodules; then
 		# unstaged changes
 		style="fg=yellow,italics"
-	elif ! git -C $1 diff-index --cached --quiet --ignore-submodules HEAD 2>/dev/null; then
+	elif ! git -C "$1" diff-index --cached --quiet --ignore-submodules HEAD 2>/dev/null; then
 		# uncommitted changes
 		style="fg=yellow"
-	elif ! git -C $1 diff --quiet origin 2>/dev/null; then
+	elif ! git -C "$1" diff --quiet "@{u}" 2>/dev/null; then
 		# unpushed commits
 		style="fg=brightred,italics"
 	fi
 
 	if [[ -n $style ]]; then
-		local name=$(basename $1)
-		printf "#[${style}]%s#[default] " "$(basename $1)"
+		local name=$(basename "$1")
+		printf "#[${style}]%s#[default] " "$(basename "$1")"
 	fi
 }
 
 function main() {
 
-	pushd $HOME > /dev/null
+	pushd "$HOME" > /dev/null || return
 
 	for i in $(cat .local/state/user_config_dirs); do
 		check-git-status "$i"
 	done
 
-	popd > /dev/null
+	popd > /dev/null || return
 }
-main $@
+main "$@"
