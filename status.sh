8277c15bd14733ff07f71137d13a5cc6a2c50184
Ganreshnu <ganreshnu@gmail.com>
2025-01-15 21:50:32 -0600

    upgrading the statusbar script

--- status.sh
+++ status.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
 
-function check-git-status() {
-	# local branch
-	# branch="$(git -C "$1" branch --show-current)"
-	local style
+gitStatusWrap() {
+	local dir="$1"; shift
+	local status="$(git -C "$dir" status --porcelain)"
+	if [[ -z "$status" ]]; then
+		status="$(git -C "$dir" status |grep -q 'branch is ahead')"
 
-	if ! git -C "$1" diff-files --quiet --ignore-submodules; then
-		# unstaged changes
-		style="fg=yellow,italics"
-	elif ! git -C "$1" diff-index --cached --quiet --ignore-submodules HEAD 2>/dev/null; then
-		# uncommitted changes
-		style="fg=yellow"
-	elif ! git -C "$1" diff --quiet "@{u}" 2>/dev/null; then
-		# unpushed commits
-		style="fg=brightred,italics"
+		git -C "$dir" status |grep -q 'branch is ahead' \
+			|| return 0
 	fi
 
-	if [[ -n $style ]]; then
-		printf "#[${style}]%s#[default] " "$(basename "$1")"
-	fi
+	local name="$(basename "$dir")"
+	local color="lightcyan"
+	printf "#[fg=$color]%s#[default]" " $name"
 }
 
+# untracked files
+# unstaged changes
+# uncommitted changes 
+# unpushed commits
 function main() {
 
 	pushd "$HOME" > /dev/null || return
 
 	while read -r d; do
-		check-git-status "$d"
+		gitStatusWrap "$d"
 	done < .local/state/user_config_dirs
 
 	popd > /dev/null || return
